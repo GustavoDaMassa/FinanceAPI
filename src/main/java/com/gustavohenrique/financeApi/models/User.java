a6e623c14cc64a26850f3e954e83400ca40893fd
Gustavo <gustavo.pereira@discente.ufg>
2025-06-17 00:19:34 -0300

    feat: new version to migrations V2 add transaction payload column

---
+++ src/main/java/com/gustavohenrique/financeApi/models/User.java
@@ -0,0 +1,11 @@
+package com.gustavohenrique.financeApi.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    private Long id
+}
