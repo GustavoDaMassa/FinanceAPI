6679bb0adbb150a83d30a9b265e5a20cfb9acdc0
Gustavo <gustavo.pereira@discente.ufg>
2025-07-12 13:45:57 -0300

    feat: definition of account and user that will receive the detected transaction

--- src/main/java/com/gustavohenrique/financeApi/webhook/consumer/SetUpWebhook.java
+++ src/main/java/com/gustavohenrique/financeApi/webhook/consumer/SetUpWebhook.java
@@ -1,4 +1,10 @@
 package com.gustavohenrique.financeApi.webhook.consumer;
 
+import com.gustavohenrique.financeApi.domain.models.Account;
+import com.gustavohenrique.financeApi.domain.models.User;
+
 public interface SetUpWebhook {
+    Long UserWebhookID (User user);
+
+    Account AccountWebhookId(Account account);
 }
--- src/main/java/com/gustavohenrique/financeApi/webhook/consumer/SetUpWebhookImpl.java
+++ src/main/java/com/gustavohenrique/financeApi/webhook/consumer/SetUpWebhookImpl.java
@@ -1,4 +1,35 @@
 package com.gustavohenrique.financeApi.webhook.consumer;
 
-public class SetUpWebhookImpl {
+import com.gustavohenrique.financeApi.application.repositories.AccountRepository;
+import com.gustavohenrique.financeApi.application.repositories.UserRepository;
+import com.gustavohenrique.financeApi.domain.models.Account;
+import com.gustavohenrique.financeApi.domain.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetUpWebhookImpl implements SetUpWebhook{
+
+    private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public SetUpWebhookImpl(UserRepository userRepository, AccountRepository accountRepository) {
+        this.userRepository = userRepository;
+        this.accountRepository = accountRepository;
+    }
+
+
+    @Override
+    public Long UserWebhookID(User user) {
+        if(userRepository.existsByEmail(user.getEmail())) return userRepository.findByEmail(user.getEmail()).orElseThrow().getId();
+        else return userRepository.save(user).getId();
+    }
+
+    @Override
+    public Account AccountWebhookId(Account account) {
+        if(accountRepository.existsByAccountName(account.getAccountName()))
+            return accountRepository.findByAccountName(account.getAccountName());
+        else return accountRepository.save(account);
+    }
 }
