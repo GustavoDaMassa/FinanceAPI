5731d137e8aa0d2798bd1cb5b4ca6e05b64a4fcc
Gustavo <gustavo.pereira@discente.ufg>
2025-07-12 13:27:45 -0300

    feat: User and account integration for webhook

--- src/main/java/com/gustavohenrique/financeApi/application/interfaces/FinancialIntegrationService.java
+++ src/main/java/com/gustavohenrique/financeApi/application/interfaces/FinancialIntegrationService.java
@@ -1,5 +1,6 @@
 package com.gustavohenrique.financeApi.application.interfaces;
 
+import com.gustavohenrique.financeApi.domain.enums.AggregatorType;
 import com.gustavohenrique.financeApi.domain.models.Account;
 import com.gustavohenrique.financeApi.domain.models.FinancialIntegration;
 import com.gustavohenrique.financeApi.domain.models.User;
@@ -11,6 +12,8 @@ import java.util.List;
 public interface FinancialIntegrationService {
     FinancialIntegration findById(Long id);
 
+    FinancialIntegration resolveIntegrationByLinkId(String linkId, Long userId, Long accountId, AggregatorType aggregator);
+
     List<FinancialIntegration> findByUserId(Long userId);
 
     FinancialIntegration create(FinancialIntegration financialIntegration, Account account);
@@ -20,4 +23,5 @@ public interface FinancialIntegrationService {
     FinancialIntegration delete(Long id);
 
     List<Account> listIntegrationAccounts(Long id);
+
 }
