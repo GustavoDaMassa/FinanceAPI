3c20bf3e383f1c39c9aef4083f08c2bd3e33b8c5
Gustavo <gustavo.pereira@discente.ufg>
2025-11-29 07:59:18 -0300

    feat: add password encryption in UserService
    Update UserServiceImpl to:
    - Encrypt passwords with BCrypt on user creation
    - Encrypt passwords on user update (if provided)

--- src/main/java/com/gustavohenrique/financeApi/application/services/UserServiceImpl.java
+++ src/main/java/com/gustavohenrique/financeApi/application/services/UserServiceImpl.java
@@ -2,12 +2,14 @@ package com.gustavohenrique.financeApi.application.services;
 
 import com.gustavohenrique.financeApi.application.interfaces.UserService;
 import com.gustavohenrique.financeApi.application.repositories.UserRepository;
+import com.gustavohenrique.financeApi.domain.enums.Role;
 import com.gustavohenrique.financeApi.domain.models.User;
 import com.gustavohenrique.financeApi.exception.EmailAlreadyExistException;
 import com.gustavohenrique.financeApi.exception.UserIDNotFoundException;
 import com.gustavohenrique.financeApi.exception.UserNotFoundException;
 import com.gustavohenrique.financeApi.graphql.inputs.UserInput;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,12 +17,13 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-        private final UserRepository userRepository;
-
-        @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -35,21 +38,37 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createUser(UserInput input) {
+        if(userRepository.existsByEmail(input.getEmail())) {
+            throw new EmailAlreadyExistException(input.getEmail());
+        }
 
-            if(userRepository.existsByEmail(input.getEmail())) throw new EmailAlreadyExistException(input.getEmail());
-            User user = new User(null,input.getName(),input.getEmail(), input.getPassword(), null,null);
+        User user = new User();
+        user.setName(input.getName());
+        user.setEmail(input.getEmail());
+        user.setPassword(passwordEncoder.encode(input.getPassword()));
+        user.setRole(Role.USER);
+
         return userRepository.save(user);
     }
 
     @Override
     public User updateUser(Long id, UserInput input) {
+        var user = userRepository.findById(id).orElseThrow(() -> new UserIDNotFoundException(id));
 
-            var user = userRepository.findById(id).orElseThrow(()-> new UserIDNotFoundException(id));
-            if(userRepository.existsByEmail(input.getEmail())) throw new EmailAlreadyExistException(input.getEmail());
-            user.setEmail(input.getEmail());
-            user.setName(input.getName());
-            user.setPassword(input.getPassword());
-            return userRepository.save(user);
+        // Only check email uniqueness if email is being changed
+        if (!user.getEmail().equals(input.getEmail()) && userRepository.existsByEmail(input.getEmail())) {
+            throw new EmailAlreadyExistException(input.getEmail());
+        }
+
+        user.setEmail(input.getEmail());
+        user.setName(input.getName());
+
+        // Only update password if provided and different
+        if (input.getPassword() != null && !input.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(input.getPassword()));
+        }
+
+        return userRepository.save(user);
     }
 
     @Override
