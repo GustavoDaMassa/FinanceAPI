d3f30361ec6dbbdff63b0c76a600a8daba9b483b
Gustavo <gustavo.pereira@discente.ufg>
2026-03-10 09:45:11 -0300

    refactor: remove repository dependency from FinancialIntegrationMapper

--- src/main/java/com/gustavohenrique/financeApi/graphql/mappers/FinancialIntegrationMapper.java
+++ src/main/java/com/gustavohenrique/financeApi/graphql/mappers/FinancialIntegrationMapper.java
@@ -1,25 +1,18 @@
 package com.gustavohenrique.financeApi.graphql.mappers;
 
-import com.gustavohenrique.financeApi.application.repositories.FinancialIntegrationRepository;
 import com.gustavohenrique.financeApi.domain.models.FinancialIntegration;
 import com.gustavohenrique.financeApi.domain.models.User;
 import com.gustavohenrique.financeApi.graphql.dtos.FinancialIntegrationDTO;
 import com.gustavohenrique.financeApi.graphql.inputs.FinancialIntegrationInput;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class FinancialIntegrationMapper {
 
     private final ModelMapper modelMapper;
-    private final FinancialIntegrationRepository financialIntegrationRepository;
-
-    @Autowired
-    public FinancialIntegrationMapper(ModelMapper modelMapper, FinancialIntegrationRepository financialIntegrationRepository) {
-        this.modelMapper = modelMapper;
-        this.financialIntegrationRepository = financialIntegrationRepository;
-    }
 
     public FinancialIntegrationDTO toDto(FinancialIntegration integration) {
         FinancialIntegrationDTO dto = modelMapper.map(integration, FinancialIntegrationDTO.class);
@@ -28,9 +21,7 @@ public class FinancialIntegrationMapper {
     }
 
     public FinancialIntegration fromInput(FinancialIntegrationInput input, User user) {
-        FinancialIntegration integration = financialIntegrationRepository.findByLinkId(input.getLinkId())
-                .orElse(new FinancialIntegration());
-
+        FinancialIntegration integration = new FinancialIntegration();
         integration.setAggregator(input.getAggregator());
         integration.setLinkId(input.getLinkId());
         integration.setUser(user);
